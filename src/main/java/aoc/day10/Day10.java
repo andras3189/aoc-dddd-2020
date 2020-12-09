f516cb25d393a2093b6c5aa6766612c0491bfe94
Andras Molnar <andras.molnar@casrd.net>
2020-12-09 07:40:11 +0100

    Day9

---
+++ src/main/java/aoc/day10/Day10.java
@@ -0,0 +1,38 @@
+package aoc.day10;
+
+import aoc.TheDayBase;
+
+public class Day10 extends TheDayBase {
+
+	public Day10(boolean multiLineInput) {
+		super(multiLineInput);
+		//input = readFile("day10/dummy");
+	}
+
+	@Override
+	protected String getInputFilename() {
+		return "day10/day10";
+	}
+
+	public static void main(String[] args) {
+		Day10 day = new Day10(false);
+		day.processInput();
+		day.puzzle1();
+		day.puzzle2();
+	}
+
+	@Override
+	protected void processInput() {
+
+	}
+
+	@Override
+	protected void puzzle1() {
+
+	}
+
+	@Override
+	protected void puzzle2() {
+
+	}
+}
--- src/main/java/aoc/day9/Day9.java
+++ src/main/java/aoc/day9/Day9.java
@@ -1,8 +1,17 @@
 package aoc.day9;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
 import aoc.TheDayBase;
 
 public class Day9 extends TheDayBase {
+
+	public static List<Long> longInput = new ArrayList<>();
+
+	private int wrongIndex = -1;
 
 	public Day9(boolean multiLineInput) {
 		super(multiLineInput);
@@ -17,22 +26,71 @@ public class Day9 extends TheDayBase {
 	public static void main(String[] args) {
 		Day9 day = new Day9(false);
 		day.processInput();
-		day.puzzle1();
-		day.puzzle2();
+		day.puzzle1(); // 36845998
+		day.puzzle2(); // 4830226
 	}
 
 	@Override
 	protected void processInput() {
-
+		System.out.println("input length = " + input.size());
+		longInput = input.stream().map(this::parseLong).collect(Collectors.toList());
 	}
 
 	@Override
 	protected void puzzle1() {
+		findWrong();
+	}
 
+	private void findWrong() {
+		int preamble = 25;
+		for (int i = preamble; i < longInput.size(); i++) {
+			long number = longInput.get(i);
+			boolean found = false;
+			for (int j = i - 1; j >= i - preamble; j--) {
+				for (int k = i - 1; k >= i - preamble; k--) {
+					if (j == k) {
+						continue;
+					}
+					if (longInput.get(j) + longInput.get(k) == number) {
+						found = true;
+						break;
+					}
+				}
+				if (found) {
+					break;
+				}
+
+			}
+			if (!found) {
+				System.out.println(number + " index = " + i);
+				wrongIndex = i;
+			}
+		}
 	}
 
 	@Override
 	protected void puzzle2() {
+		int searchLength = 100;
+		for (int i = 0; i < longInput.size(); i++) {
+			if (i == wrongIndex) {
+				continue;
+			}
 
+			long sum = 0;
+			for (int j = i; j < Math.min(i + searchLength, longInput.size()); j++) {
+				if (j == wrongIndex) {
+					continue;
+				}
+
+				sum += parseLong(input.get(j));
+
+				if (sum == longInput.get(wrongIndex)) {
+					long min = LongStream.range(i, j + 1).map(k -> longInput.get((int) k)).min().orElseThrow();
+					long max = LongStream.range(i, j + 1).map(k -> longInput.get((int) k)).max().orElseThrow();
+					System.out.println("sum of min + max = " + (min + max) + " length = " + (j - i + 1));
+					LongStream.range(i,j+1).map(k -> longInput.get((int) k)).forEach(System.out::println);
+				}
+			}
+		}
 	}
 }
