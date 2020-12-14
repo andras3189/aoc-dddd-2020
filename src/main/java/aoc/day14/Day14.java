0aa13a03015cd0d74e9d1d387b7afa1d8f5e581e
Andras Molnar <andras.molnar@casrd.net>
2020-12-14 16:12:43 +0100

    Day14 2

--- src/main/java/aoc/day14/Day14.java
+++ src/main/java/aoc/day14/Day14.java
@@ -17,8 +17,8 @@ public class Day14 extends DayBase {
 		new Day14().run();
 	}
 
-	public Map<Long, Long> memory = new HashMap<>();
-
+	private final Map<Long, Long> memory = new HashMap<>();
+	private final Map<Integer, List<String>> maskMemory = new HashMap<>();
 	public String mask = "";
 
 	@Override
@@ -30,128 +30,108 @@ public class Day14 extends DayBase {
 	@Override
 	protected void processInput() {
 
-		memory.clear();
-		for (String line : input) {
-			if (line.startsWith("mask")) {
-				setMask(line);
-			} else {
-				writeMem(line);
-			}
-		}
 	}
 
-	private void writeMem(String line) {
-		Pattern pattern = Pattern.compile("mem\\[(\\d*)\\] = (\\d*)");
-		Matcher matcher = pattern.matcher(line);
-		if (matcher.find()) {
-			long index = parseLong(matcher.group(1));
-			long value = parseLong(matcher.group(2));
-			value = applyMask(value,mask);
-			memory.put(index, value);
-		}
+	@Override
+	protected void puzzle1() {
+		solvePuzzle(false);
 	}
 
 	@Override
 	protected void puzzle2() {
-
-		memory.clear();
-		for (String line : input) {
-			if (line.startsWith("mask")) {
-				setMask(line);
-			} else {
-				writeMem2(line);
-			}
-		}
-
-		long sum = memory.keySet().stream().map(key -> memory.get(key)).mapToLong(Long::longValue).sum();
-		System.out.println(sum);
-
+		solvePuzzle(true);
 	}
 
-	private void writeMem2(String line) {
+	private void solvePuzzle(boolean floatingMask) {
+		for (String line : input) {
+			setMask(line);
+			writeMem(line, floatingMask);
+		}
+		long sum = memory.keySet().stream().map(memory::get).mapToLong(Long::longValue).sum();
+		System.out.println(sum);
+	}
+
+	private void writeMem(String line, boolean floating) {
 		Pattern pattern = Pattern.compile("mem\\[(\\d*)\\] = (\\d*)");
 		Matcher matcher = pattern.matcher(line);
 		if (matcher.find()) {
 			long index = parseLong(matcher.group(1));
 			long value = parseLong(matcher.group(2));
 
-			for (Long i : applyMask2(index)) {
-				memory.put(i, value);
+			if (floating) {
+				for (Long i : applyFloatingMask(index)) {
+					memory.put(i, value);
+				}
+			} else {
+				value = applyMask(value, mask);
+				memory.put(index, value);
 			}
 		}
 	}
 
-	private List<Long> applyMask2(long value) {
+	private List<Long> applyFloatingMask(long value) {
 		List<Long> result = new ArrayList<>();
-		long value2 = applyMask(value, mask);
-		List<String> masks = generateMasks(0);
-
-		for (String lMask : masks) {
-			result.add(applyMask(value2, lMask));
+		maskMemory.clear();
+		for (String mask : generateMasks(0)) {
+			result.add(applyMask(value, mask));
 		}
-
-		return result;
-	}
-
-	private Map<Integer, List<String>> memoryMasks = new HashMap<>();
-
-	private List<String> generateMasks(int index) {
-		List<Integer> collect = mask.chars().boxed().collect(Collectors.toList());
-		List<String> result = new ArrayList<>();
-		Integer c = collect.get(index);
-
-		System.out.println(index);
-
-		if (memoryMasks.containsKey(index)) {
-			return memoryMasks.get(index);
-		}
-
-		if (index == 35) {
-			if (c == '0') {
-				result.add("X");
-			}
-			if (c == '1') {
-				result.add("X");
-			}
-			if (c == 'X') {
-				result.add("0");
-				result.add("1");
-			}
-		} else if (c == '0' || c == '1') {
-			List<String> generated = generateMasks(index + 1);
-			generated.forEach(g -> result.add("X" + g));
-		} else if (c == 'X') {
-			List<String> generated = generateMasks(index + 1);
-			generated.forEach(g -> result.add("0" + g));
-			generated.forEach(g -> result.add("1" + g));
-		}
-
-		memoryMasks.put(index, result);
-
-		System.out.println("index done " + index);
 		return result;
 	}
 
 	private long applyMask(long value, String mask) {
-		List<Integer> collect = mask.chars().boxed().collect(Collectors.toList());
-		for (int i = 0; i < collect.size(); i++) {
-			Integer c = collect.get(i);
+		List<Integer> maskCharacters = mask.chars().boxed().collect(Collectors.toList());
+		for (int i = maskCharacters.size() - 1; i >= 0; i--) {
+			Integer c = maskCharacters.get(maskCharacters.size() - i - 1);
 			if (c == 'X') {
 				continue;
 			}
 			if (c == '1') {
-				long binary = 1L << (collect.size() - i - 1);
+				long binary = 1L << i;
 				value |= binary;
 			}
 			if (c == '0') {
-				long binary;
-				binary = 1L << (collect.size() - i - 1L);
-				binary = binary & ~(1L << 63);
+				long binary = 1L << i;
 				binary = ~binary & ~(1L << 63);
 				value &= binary;
 			}
 		}
 		return value;
+	}
+
+	private List<String> generateMasks(int start) {
+		List<String> result = new ArrayList<>();
+		List<Integer> maskCharacters = mask.chars().boxed().collect(Collectors.toList());
+		Integer c = maskCharacters.get(start);
+
+		if (maskMemory.containsKey(start)) {
+			return maskMemory.get(start);
+		}
+
+		if (start == mask.length() - 1) {
+			if (c == '0') {
+				result.add("X");
+			}
+			if (c == '1') {
+				result.add("1");
+			}
+			if (c == 'X') {
+				result.add("0");
+				result.add("1");
+			}
+		} else if (c == '0') {
+			List<String> generated = generateMasks(start + 1);
+			generated.forEach(g -> result.add("X" + g));
+		} else if (c == '1') {
+			List<String> generated = generateMasks(start + 1);
+			generated.forEach(g -> result.add("1" + g));
+		} else if (c == 'X') {
+			List<String> generated = generateMasks(start + 1);
+			generated.forEach(g -> result.add("0" + g));
+			generated.forEach(g -> result.add("1" + g));
+		}
+
+		maskMemory.put(start, result);
+		return result;
 	}
 
 	private void setMask(String line) {
@@ -164,16 +144,6 @@ public class Day14 extends DayBase {
 
 	@Override
 	protected void reset() {
-	}
-
-	@Override
-	protected void puzzle1() {
-		long sum = memory.keySet().stream().map(key -> memory.get(key)).mapToLong(Long::longValue).sum();
-		System.out.println(sum);
-	}
-
-	public static class Memory {
-		long index;
-		long value;
+		memory.clear();
 	}
 }
