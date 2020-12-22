bc00e3515590197ecdfe563cb079cc193870459c
Andras Molnar <andras.molnar@casrd.net>
2020-12-22 10:32:31 +0100

    Day22 2

--- src/main/java/aoc/day22/Day22.java
+++ src/main/java/aoc/day22/Day22.java
@@ -1,7 +1,11 @@
 package aoc.day22;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import aoc.DayBase;
 
@@ -14,14 +18,16 @@ public class Day22 extends DayBase {
 		day.run();
 	}
 
-	private List<Integer> p1 = new ArrayList<>();
-	private List<Integer> p2 = new ArrayList<>();
+	private final List<Integer> inputPlayer1 = new ArrayList<>();
+	private final List<Integer> inputPlayer2 = new ArrayList<>();
+	private final Map<String, Integer> memory = new HashMap<>();
+
 
 	@Override
 	protected void processInput() {
 		int player = 1;
-		p1.clear();
-		p2.clear();
+		inputPlayer1.clear();
+		inputPlayer2.clear();
 
 		for (String line : input) {
 			if ("".equals(line)) {
@@ -32,22 +38,22 @@ public class Day22 extends DayBase {
 			} else if (line.equals("Player 2:")) {
 				player = 2;
 			} else if (player == 1) {
-				p1.add(parseInt(line));
-			} else if (player == 2) {
-				p2.add(parseInt(line));
+				inputPlayer1.add(parseInt(line));
+			} else {
+				inputPlayer2.add(parseInt(line));
 			}
 		}
-
 	}
 
 	@Override
 	protected void reset() {
-
+		memory.clear();
 	}
 
 	@Override
 	protected void puzzle1() {
-		System.out.println();
+		List<Integer> p1 = new ArrayList<>(this.inputPlayer1);
+		List<Integer> p2 = new ArrayList<>(this.inputPlayer2);
 
 		while (p1.size() != 0 && p2.size() != 0) {
 			int c1 = p1.get(0);
@@ -63,25 +69,93 @@ public class Day22 extends DayBase {
 			p1.remove(0);
 			p2.remove(0);
 		}
-		System.out.println();
-
-		long sum = 0;
-		if (p2.size() > 0) {
-			for (int i = 0; i < p2.size() ; i++) {
-				sum += (long) p2.get(i) * (p2.size()-i);
-			}
-		}
-
-		if (p1.size() > 0) {
-			for (int i = 0; i < p1.size() ; i++) {
-				sum += (long) p1.get(i) * (p1.size()-i);
-			}
-		}
-		System.out.println(sum);
+		printSum(p1, p2);
 	}
 
 	@Override
 	protected void puzzle2() {
+		List<Integer> p1 = new ArrayList<>(this.inputPlayer1);
+		List<Integer> p2 = new ArrayList<>(this.inputPlayer2);
 
+		playCards(p1, p2);
+		printSum(p1, p2);
+	}
+
+	private int playCards(List<Integer> p1, List<Integer> p2) {
+		String gameHash = hash(p1, p2);
+
+		if (memory.containsKey(gameHash)) {
+			return memory.get(gameHash);
+		}
+
+		Set<String> previousRounds = new HashSet<>();
+		while (p1.size() != 0 && p2.size() != 0) {
+			int roundWinner;
+
+			String roundHash = hash(p1, p2);
+			int c1 = p1.remove(0);
+			int c2 = p2.remove(0);
+
+			if (previousRounds.contains(roundHash)) {
+				return 1;
+			}
+			previousRounds.add(roundHash);
+
+			if (p1.size() >= c1 && p2.size() >= c2) {
+				List<Integer> np1 = copy(p1, c1);
+				List<Integer> np2 = copy(p2, c2);
+				roundWinner = playCards(np1, np2);
+			} else {
+				roundWinner = c1 > c2 ? 1 : 2;
+			}
+
+			if (roundWinner == 1) {
+				p1.add(c1);
+				p1.add(c2);
+			} else {
+				p2.add(c2);
+				p2.add(c1);
+			}
+		}
+		memory.put(gameHash, p1.size() > 0 ? 1 : 2);
+		return memory.get(gameHash);
+	}
+
+	private List<Integer> copy(List<Integer> source, int count) {
+		List<Integer> copy = new ArrayList<>();
+		for (int i = 0; i < count; i++) {
+			copy.add(source.get(i));
+		}
+		return copy;
+	}
+
+	private String hash(List<Integer> p1, List<Integer> p2) {
+		StringBuilder b = new StringBuilder();
+		for (Integer c : p1) {
+			b.append(c);
+			b.append(",");
+		}
+		b.append("-");
+		for (Integer c : p2) {
+			b.append(c);
+			b.append(",");
+		}
+		return b.toString();
+	}
+
+	private void printSum(List<Integer> p1, List<Integer> p2) {
+		long sum = 0;
+		if (p2.size() > 0) {
+			for (int i = 0; i < p2.size(); i++) {
+				sum += (long) p2.get(i) * (p2.size() - i);
+			}
+		}
+
+		if (p1.size() > 0) {
+			for (int i = 0; i < p1.size(); i++) {
+				sum += (long) p1.get(i) * (p1.size() - i);
+			}
+		}
+		System.out.println(sum);
 	}
 }
