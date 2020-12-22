d6f7c764ed52c5d7da424b6c10c4d1a9072fe158
David Haas Muñoz <david.haas.munoz@cas.de>
2020-12-22 07:59:25 +0100

    Day 22 - partial solution by David

--- src/main/java/aoc/day22/Day22.java
+++ src/main/java/aoc/day22/Day22.java
@@ -2,6 +2,7 @@ package aoc.day22;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import aoc.DayBase;
 
@@ -9,19 +10,22 @@ public class Day22 extends DayBase {
 
 	public static void main(String[] args) {
 		Day22 day = new Day22();
-//		day.setOnlyDummyInput(true);
-//		day.setOnlyRealInput(true);
+		// day.setOnlyDummyInput(true);
+		// day.setOnlyRealInput(true);
 		day.run();
 	}
 
-	private List<Integer> p1 = new ArrayList<>();
-	private List<Integer> p2 = new ArrayList<>();
+	private List<Integer> deck1 = new ArrayList<>();
+	private List<Integer> deck2 = new ArrayList<>();
+	private List<Integer> sub1 = new ArrayList<>();
+	private List<Integer> sub2 = new ArrayList<>();
+	private List<List<String>> allStates = new ArrayList<>();
 
 	@Override
 	protected void processInput() {
 		int player = 1;
-		p1.clear();
-		p2.clear();
+		deck1.clear();
+		deck2.clear();
 
 		for (String line : input) {
 			if ("".equals(line)) {
@@ -32,9 +36,9 @@ public class Day22 extends DayBase {
 			} else if (line.equals("Player 2:")) {
 				player = 2;
 			} else if (player == 1) {
-				p1.add(parseInt(line));
+				deck1.add(parseInt(line));
 			} else if (player == 2) {
-				p2.add(parseInt(line));
+				deck2.add(parseInt(line));
 			}
 		}
 
@@ -42,46 +46,107 @@ public class Day22 extends DayBase {
 
 	@Override
 	protected void reset() {
-
+		this.processInput();
 	}
 
 	@Override
 	protected void puzzle1() {
-		System.out.println();
+		// System.out.println();
 
-		while (p1.size() != 0 && p2.size() != 0) {
-			int c1 = p1.get(0);
-			int c2 = p2.get(0);
+		while (deck1.size() != 0 && deck2.size() != 0) {
+			int c1 = deck1.get(0);
+			int c2 = deck2.get(0);
 
 			if (c1 > c2) {
-				p1.add(c1);
-				p1.add(c2);
+				deck1.add(c1);
+				deck1.add(c2);
 			} else {
-				p2.add(c2);
-				p2.add(c1);
+				deck2.add(c2);
+				deck2.add(c1);
 			}
-			p1.remove(0);
-			p2.remove(0);
+			deck1.remove(0);
+			deck2.remove(0);
 		}
-		System.out.println();
+		// System.out.println();
 
 		long sum = 0;
-		if (p2.size() > 0) {
-			for (int i = 0; i < p2.size() ; i++) {
-				sum += (long) p2.get(i) * (p2.size()-i);
+		if (deck2.size() > 0) {
+			for (int i = 0; i < deck2.size(); i++) {
+				sum += (long) deck2.get(i) * (deck2.size() - i);
 			}
 		}
 
-		if (p1.size() > 0) {
-			for (int i = 0; i < p1.size() ; i++) {
-				sum += (long) p1.get(i) * (p1.size()-i);
+		if (deck1.size() > 0) {
+			for (int i = 0; i < deck1.size(); i++) {
+				sum += (long) deck1.get(i) * (deck1.size() - i);
 			}
 		}
-		System.out.println(sum);
+		// System.out.println(sum);
 	}
 
 	@Override
 	protected void puzzle2() {
+		int winner = recCombat(deck1, deck2, 0);
 
+		long sum = 0;
+		if (winner == 2) {
+			for (int i = 0; i < deck2.size(); i++) {
+				sum += (long) deck2.get(i) * (deck2.size() - i);
+			}
+		}
+
+		if (winner == 1) {
+			for (int i = 0; i < deck1.size(); i++) {
+				sum += (long) deck1.get(i) * (deck1.size() - i);
+			}
+		}
+		System.out.println(sum);
+
+	}
+
+	private int recCombat(List<Integer> deck1, List<Integer> deck2, int depth) {
+
+		allStates.add(new ArrayList<>());
+		List<String> gameStates = allStates.get(allStates.size()-1);
+
+		int round = 0;
+		while (deck1.size() != 0 && deck2.size() != 0) {
+			// System.out.println("Game " + allStates.size() + ", Round " + round);
+
+			String config = deck1.toString() + "|" + deck2.toString();
+			if (gameStates.contains(config)) {
+				return 1;
+			}
+			gameStates.add(config);
+
+			int c1 = deck1.get(0);
+			int c2 = deck2.get(0);
+
+			sub1 = deck1.subList(1, deck1.size()).stream().collect(Collectors.toList());
+			sub2 = deck2.subList(1, deck2.size()).stream().collect(Collectors.toList());
+
+			int roundWinner = 0;
+			if (sub1.size() >= c1 && sub2.size() >= c2) {
+				roundWinner = recCombat(sub1, sub2, depth + 1);
+			} else {
+				roundWinner = c1 > c2 ? 1 : 2;
+			}
+
+			if (roundWinner == 1) {
+				deck1.add(c1);
+				deck1.add(c2);
+			} else if (roundWinner == 2) {
+				deck2.add(c2);
+				deck2.add(c1);
+			} else {
+				System.out.println("Youdonefuckedup! Winner is " + roundWinner);
+			}
+			deck1.remove(0);
+			deck2.remove(0);
+			round++;
+		}
+
+		int gameWinner = deck2.size() == 0 ? 1 : 2;
+		return gameWinner;
 	}
 }
